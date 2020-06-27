e0355daae667722d763823618ee8b2f342fdebff
CaseyBandiola <45117883+CaseyBandiola@users.noreply.github.com>
2020-06-28 02:41:21 +0800

    Added new features
    - Cheems-Bot now handles arguments with a missing closing quotation mark
    - Changed command syntax from underscore to hyphen
    - Cheems-Bot has a new command: ``` $cheems-be-wholesome ```
    - Fixed some redundant code

--- bot.py
+++ bot.py
@@ -11,10 +11,30 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
+# Shows that Cheems is online
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is online!')
+
 # Tell cheems to say something
-@bot.command(name='cheems_say', help='Cheems will say what you want him to say.')
+@bot.command(name='cheems-say', help='Cheems will say what you want him to say.')
 async def say(ctx, msg):
     response = cheemsify(msg)
+    await ctx.send(response)
+
+# Tell cheems to say something wholesome
+@bot.command(name='cheems-be-wholesome', help='Cheems will say something wholesome.')
+async def wholesome(ctx):
+    wholesome_quotes = [
+        cheemsify("I love you"),
+        cheemsify("You matter"),
+        cheemsify("Have a nice day"),
+        cheemsify("Don't forget to drink water today"),
+        cheemsify("Did you eat enough today?"),
+        cheemsify("Hope you have a good day!"),
+    ]
+
+    response = random.choice(wholesome_quotes)
     await ctx.send(response)
 
 # Cheemsifies the message
@@ -34,7 +54,7 @@ def cheemsify(msg):
             continue
         # otherwise, cheemsify the word
         for j in range( len(response[i])-1 ):
-            if isVowel(response[i][j]) and isConsonant(response[i][j+1]) and response[i][j+1] != 'm':
+            if isVowel(response[i][j]) and isConsonant(response[i][j+1]):
                 new_word = response[i][:j+1]+"m"+response[i][j+1:]
                 response[i] = new_word
                 break
@@ -50,6 +70,12 @@ def isVowel(char):
     return char.lower() in 'aeiou'
 
 def isConsonant(char):
-    return char.lower() in 'bcdfghjklmnpqrstvxzwy'
+    return char.lower() in 'bcdfghjklnpqrstvxzwy'
+
+# error handling for say()
+@say.error
+async def say_error(ctx, error):
+    if isinstance(error, commands.CommandError):
+        await ctx.send('I canm\'t say thamt')
 
 bot.run(TOKEN)
